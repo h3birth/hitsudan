9585b779f15800320b57621f87bca4de05fbb777
kako351 <h3.birth@gmail.com>
2021-06-20 22:43:15 +0900

    set landscape

--- app/app/src/main/java/app/birth/h3/MainActivity.kt
+++ app/app/src/main/java/app/birth/h3/MainActivity.kt
@@ -8,6 +8,7 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.content.res.Configuration
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.os.Bundle
